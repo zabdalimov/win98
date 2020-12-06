b90e8a5cb9a4f66c45d3ee5a47da2aee4aa1e1ed
zabdalimov <zabdalimov@gmail.com>
2020-12-07 00:07:51 +0100

    task bar separator added

---
+++ src/components/TaskBar/Separator/Separator.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Separator.css'
+
+const Separator: React.FC = () => {
+  return <div className="Separator" />
+}
+
+export default Separator
--- src/components/TaskBar/TaskBar.tsx
+++ src/components/TaskBar/TaskBar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './TaskBar.css'
 import StartButton from './StartButton/StartButton'
+import Separator from './Separator/Separator'
 
 const TaskBar: React.FC = () => {
   return (
     <div className="TaskBar">
       <StartButton />
+      <Separator />
     </div>
   )
 }
