59d9df26ff2c935946660628d3702060bc7a0c19
zabdalimov <zabdalimov@gmail.com>
2020-12-12 01:25:45 +0100

    StartMenu base added

--- src/components/TaskBar/StartButton/StartButton.tsx
+++ src/components/TaskBar/StartButton/StartButton.tsx
@@ -3,14 +3,26 @@ import './StartButton.scss'
 import logo from '../../../static/icons/w98-icon.png'
 import Button from '../../Button/Button'
 import Icon from '../../Icon/Icon'
+import StartMenu from '../StartMenu/StartMenu'
+import {
+  clickOutsideWrapper,
+  ClickOutsideWrapperProps,
+} from '../../helpers/clickOutsideWrapper'
 
-const StartButton: React.FC = () => {
+const StartButton: React.FC<ClickOutsideWrapperProps> = ({
+  isOpen,
+  toggleIsOpen,
+  wrapperRef,
+}) => {
   return (
-    <Button className="StartButton">
-      <Icon src={logo} alt="Windows 98 Icon" />
-      <span className="StartButtonText">Start</span>
-    </Button>
+    <div ref={wrapperRef}>
+      {isOpen && <StartMenu />}
+      <Button className="StartButton" onClick={toggleIsOpen}>
+        <Icon src={logo} alt="Windows 98 Icon" />
+        <span className="StartButtonText">Start</span>
+      </Button>
+    </div>
   )
 }
 
-export default StartButton
+export default clickOutsideWrapper(StartButton)
---
+++ src/components/TaskBar/StartMenu/StartMenu.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './StartMenu.scss'
+
+const StartMenu: React.FC = () => {
+  return <div className="StartMenu">content</div>
+}
+
+export default StartMenu
