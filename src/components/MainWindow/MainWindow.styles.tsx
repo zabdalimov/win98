ed41f327c6b93e8992e817b1799b0197cc4d95ee
zabdalimov <zabdalimov@gmail.com>
2020-12-22 22:22:21 +0100

    max dimensions set for main window

--- src/components/MainWindow/MainWindow.styles.tsx
+++ src/components/MainWindow/MainWindow.styles.tsx
@@ -2,7 +2,10 @@ import colors from '../../styles/colors.styles'
 import { css } from '@emotion/react'
 
 const mainWindowStyles = css`
+  width: 100%;
   height: 100%;
+  max-width: 1024px;
+  max-height: 768px;
   position: relative;
   display: flex;
   flex-direction: column;
