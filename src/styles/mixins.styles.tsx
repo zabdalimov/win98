4e56e95bc2b38ca4f63c2213f3b6c9c35bc6680b
zabdalimov <zabdalimov@gmail.com>
2020-12-31 00:49:07 +0100

    default padding added to standardBorder and standardBorderInverted

--- src/styles/mixins.styles.tsx
+++ src/styles/mixins.styles.tsx
@@ -1,15 +1,17 @@
 import { css } from '@emotion/react'
 import colors from './colors.styles'
 
-// TODO type this out
 const mixins = {
+  // Padding is added so box shadow is not overlapping with contents of the element
   standardBorder: css`
     box-shadow: inset 1px 1px ${colors.white}, inset 2px 2px ${colors.lightGray},
       inset -1px -1px ${colors.black}, inset -2px -2px ${colors.darkGray};
+    padding: 2px;
   `,
   standardBorderInverted: css`
     box-shadow: inset 1px 1px ${colors.black}, inset 2px 2px ${colors.darkGray},
       inset -1px -1px ${colors.white}, inset -2px -2px ${colors.lightGray};
+    padding: 2px;
   `,
   dottedBorderBlack: css`
     outline: 1px dotted ${colors.black};
