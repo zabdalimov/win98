d2dad0eccb7db362d78cfebeedbf8cb5fa5bbde5
zabdalimov <zabdalimov@gmail.com>
2020-12-31 01:11:46 +0100

    onClose added to ApplicationWindow component

--- src/components/ApplicationWindow/ApplicationWindow.tsx
+++ src/components/ApplicationWindow/ApplicationWindow.tsx
@@ -6,14 +6,19 @@ import closeButtonIcon from '../../static/icons/close-button-icon.png'
 
 interface Props {
   applicationName: string
+  onClose: () => void
 }
 
-const ApplicationWindow: React.FC<Props> = ({ applicationName, children }) => {
+const ApplicationWindow: React.FC<Props> = ({
+  applicationName,
+  children,
+  onClose,
+}) => {
   return (
     <div css={styles.applicationWindow}>
       <div css={styles.applicationWindowHeader}>
         <span>{applicationName}</span>
-        <Button>
+        <Button onClick={onClose}>
           <Icon src={closeButtonIcon} alt={'Close button'} />
         </Button>
       </div>
