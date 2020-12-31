cb48aedd685e64f13f7ebff688b31fd4156c2cf4
zabdalimov <zabdalimov@gmail.com>
2020-12-31 01:12:28 +0100

    dummy application windows added when clicking on desktop icons

--- src/components/DesktopGrid/DesktopGrid.tsx
+++ src/components/DesktopGrid/DesktopGrid.tsx
@@ -4,6 +4,8 @@ import computerExplorerIcon from '../../static/icons/computer-explorer-icon.png'
 import recycleBinEmptyIcon from '../../static/icons/recycle-bin-empty-icon.png'
 import ieIcon from '../../static/icons/ie-icon.png'
 import Icon from '../Icon/Icon'
+import { css } from '@emotion/react'
+import useApplicationWindow from '../../hooks/useApplicationWindow'
 
 interface DesktopIcon {
   icon: string
@@ -11,6 +13,8 @@ interface DesktopIcon {
 }
 
 const DesktopGrid: React.FC = () => {
+  const { openApplicationWindow } = useApplicationWindow()
+
   // TODO adjust margins on these
   const icons: DesktopIcon[] = [
     {
@@ -26,10 +30,28 @@ const DesktopGrid: React.FC = () => {
       label: 'Internet Explorer',
     },
   ]
+
+  const onDoubleClick = (applicationName: string) => {
+    // TODO replace with real one
+    const dummyContent = (
+      <div
+        css={css`
+          width: 200px;
+          height: 100px;
+        `}
+      />
+    )
+    openApplicationWindow(applicationName, dummyContent)
+  }
+
   return (
     <div css={desktopGridStyles.desktopGrid}>
       {icons.map((icon) => (
-        <button key={icon.label} css={desktopGridStyles.desktopGridEntry}>
+        <button
+          key={icon.label}
+          css={desktopGridStyles.desktopGridEntry}
+          onDoubleClick={() => onDoubleClick(icon.label)}
+        >
           <Icon src={icon.icon} alt={icon.label} />
           <span>{icon.label}</span>
         </button>
