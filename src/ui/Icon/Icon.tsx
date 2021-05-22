8c9870d2426c30916fad91ba7f585c5d2d8a2471
zabdalimov <zabdalimov@gmail.com>
2021-05-22 23:40:53 +0200

    draggable param added to Icon props

--- src/ui/Icon/Icon.tsx
+++ src/ui/Icon/Icon.tsx
@@ -4,10 +4,11 @@ import { IconStyled } from './Icon.styles'
 export interface IconProps {
   src: string
   alt: string
+  draggable?: boolean
 }
 
-const Icon: React.FC<IconProps> = ({ src, alt }) => {
-  return <IconStyled src={src} alt={alt} draggable={false} />
+const Icon: React.FC<IconProps> = ({ src, alt, draggable = false }) => {
+  return <IconStyled src={src} alt={alt} draggable={draggable} />
 }
 
 export default Icon
