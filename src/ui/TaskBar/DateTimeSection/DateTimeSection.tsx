10b8f5c348a79e1553642090b1ac0d1d0e888c2f
zabdalimov <zabdalimov@gmail.com>
2021-05-23 18:18:06 +0200

    ding sound added after sound icon is clicked in taskbar

--- src/ui/TaskBar/DateTimeSection/DateTimeSection.tsx
+++ src/ui/TaskBar/DateTimeSection/DateTimeSection.tsx
@@ -2,18 +2,25 @@ import React from 'react'
 import Icon, { IconProps } from '../../Icon/Icon'
 import soundIcon from '../../../static/icons/sound-icon.png'
 import dateTimeSectionStyles from './DateTimeSection.styles'
+import { playAudio } from '../../../audio'
 
 interface Props {
   date: Date
 }
 
 const DateTimeSection: React.FC<Props> = ({ date }) => {
-  const icons: IconProps[] = [{ src: soundIcon, alt: 'Sound' }]
+  const icons: IconProps[] = [
+    {
+      src: soundIcon,
+      alt: 'Sound',
+      onClick: () => playAudio('ding.mp3'),
+    },
+  ]
 
   return (
     <div css={dateTimeSectionStyles.dateTimeSection}>
-      {icons.map((i) => (
-        <Icon key={i.src} src={i.src} alt={i.alt} />
+      {icons.map((ip) => (
+        <Icon key={ip.src} {...ip} />
       ))}
       <div css={dateTimeSectionStyles.time}>
         {date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
