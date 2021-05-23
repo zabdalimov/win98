97fd8b980e2b5e331bdba895545861bedb6de074
zabdalimov <zabdalimov@gmail.com>
2021-05-23 16:22:36 +0200

    onClick parameter added to Icon props

--- src/ui/Icon/Icon.tsx
+++ src/ui/Icon/Icon.tsx
@@ -5,10 +5,18 @@ export interface IconProps {
   src: string
   alt: string
   draggable?: boolean
+  onClick?: (event?: React.MouseEvent<HTMLImageElement>) => void
 }
 
-const Icon: React.FC<IconProps> = ({ src, alt, draggable = false }) => {
-  return <IconStyled src={src} alt={alt} draggable={draggable} />
+const Icon: React.FC<IconProps> = ({
+  src,
+  alt,
+  draggable = false,
+  onClick,
+}) => {
+  return (
+    <IconStyled src={src} alt={alt} draggable={draggable} onClick={onClick} />
+  )
 }
 
 export default Icon
