b3c100b8e556fa57d84a1b66d3438d12f480c52c
Zakir Abdalimov <zabdalimov@gmail.com>
2021-11-14 22:15:44 +0100

    basic blue screen for uncaught errors

---
+++ src/ui/ErrorBoundary/BlueScreen.styles.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+import colors from '../../styles/colors.styles'
+
+// TODO there is a different font here
+export const BlueScreenStyled = styled.div`
+  width: 100%;
+  height: 100%;
+
+  padding: 0 2rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${colors.lightBlue};
+`
+
+export const BlueScreenHeading = styled.p`
+  color: ${colors.lightBlue};
+  background-color: ${colors.baseGray};
+  padding: 0.25rem;
+  margin-bottom: 0.5rem;
+`
+
+export const BlueScreenText = styled.span`
+  color: ${colors.white};
+`
---
+++ src/ui/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { BlueScreen } from './BlueScreen'
+import { getFirstPathFromStackTrace } from '../../utils/error'
+
+interface State {
+  error: Error | null
+}
+
+// For now there is only class based implementation for ErrorBoundaries in React unfortunately
+export class ErrorBoundary extends React.Component<unknown, State> {
+  constructor(props: unknown) {
+    super(props)
+    this.state = { error: null }
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { error }
+  }
+
+  render() {
+    if (this.state.error) {
+      const errorMessage = this.state.error.message
+      const place = this.state.error.stack
+        ? getFirstPathFromStackTrace(this.state.error.stack)
+        : undefined
+      // TODO add feature to continue working without refresh (continue on any key + add relevant text)
+      // TODO refresh page shortcut based on OS
+      const body = (
+        <React.Fragment>
+          Fatal exception &quot;{errorMessage}&quot; occurred
+          {place ? ` ${place}` : ''}. Current application will be terminated.
+          <ul>
+            <li>
+              Press CTRL + F5 to terminate the current application. You will
+              lose any unsaved information in all applications.
+            </li>
+          </ul>
+        </React.Fragment>
+      )
+      return <BlueScreen body={body} />
+    }
+
+    return this.props.children
+  }
+}
