0cb88ee30f98574a6199c9e8c8a89607b09c81e2
Zakir Abdalimov <zabdalimov@gmail.com>
2021-11-17 14:33:07 +0100

    TaskBar rewritten to styled

--- src/components/TaskBar/TaskBar.styles.tsx
+++ src/components/TaskBar/TaskBar.styles.tsx
@@ -1,30 +1,27 @@
-import { css } from '@emotion/react'
 import sizings from '../../styles/sizings.styles'
 import colors from '../../styles/colors.styles'
 import SeparatorStyled from '../Separator/Separator.styles'
+import styled from '@emotion/styled'
 
-const taskBarStyles = {
-  taskBar: css`
-    height: ${sizings.taskbarHeight};
-    display: flex;
-    align-items: center;
-    margin-top: auto;
-    padding: 4px 2px 2px 2px;
-    background-color: ${colors.baseGray};
-    box-shadow: inset 0 1px ${colors.lightGray}, inset 0 2px ${colors.white};
+export const TaskBarStyled = styled.div`
+  height: ${sizings.taskbarHeight};
+  display: flex;
+  align-items: center;
+  margin-top: auto;
+  padding: 4px 2px 2px 2px;
+  background-color: ${colors.baseGray};
+  box-shadow: inset 0 1px ${colors.lightGray}, inset 0 2px ${colors.white};
 
-    > ${SeparatorStyled} {
-      margin-left: 2px;
+  > ${SeparatorStyled} {
+    margin-left: 2px;
 
-      &:nth-last-of-type(2) {
-        margin-left: auto;
-      }
+    &:nth-last-of-type(2) {
+      margin-left: auto;
     }
+  }
 
-    > div:last-of-type {
-      margin-left: 2px;
-    }
-  `,
-}
-
-export default taskBarStyles
+  // TODO use DateTimeSection after it's rewritten to styled
+  > div:last-of-type {
+    margin-left: 2px;
+  }
+`
--- src/components/TaskBar/TaskBar.tsx
+++ src/components/TaskBar/TaskBar.tsx
@@ -3,12 +3,12 @@ import StartButton from './StartButton/StartButton'
 import Separator from '../Separator/Separator'
 import DateTimeSection from './DateTimeSection/DateTimeSection'
 import useCurrentDate from '../../hooks/useCurrentDate'
-import taskBarStyles from './TaskBar.styles'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectOpenedApplications } from '../../store/application/selectors'
 import { changeApplicationFocus } from '../../store/application/actions'
 import TaskEntry from './TaskEntry/TaskEntry'
 import { Application } from '../../store/application/reducer'
+import { TaskBarStyled } from './TaskBar.styles'
 
 const TaskBar: React.FC = () => {
   const applications = useSelector(selectOpenedApplications)
@@ -20,7 +20,7 @@ const TaskBar: React.FC = () => {
   const currentDate = useCurrentDate()
 
   return (
-    <div css={taskBarStyles.taskBar}>
+    <TaskBarStyled>
       <StartButton />
       <Separator orientation={'vertical'} />
       {applications.map((a) => (
@@ -32,7 +32,7 @@ const TaskBar: React.FC = () => {
       ))}
       <Separator orientation={'vertical'} />
       <DateTimeSection date={currentDate} />
-    </div>
+    </TaskBarStyled>
   )
 }
 
