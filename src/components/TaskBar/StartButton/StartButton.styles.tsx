538355e937ebb951741e624505d18af02253404d
Zakir Abdalimov <zabdalimov@gmail.com>
2021-11-17 15:48:38 +0100

    StartButtonStyled note added

--- src/components/TaskBar/StartButton/StartButton.styles.tsx
+++ src/components/TaskBar/StartButton/StartButton.styles.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import { ButtonStyled } from '../../Button/Button.styles'
 
+// When trying to style component from the outside, use this instead of StartButtonStyled
 export const StartButtonContainer = styled.div`
   height: 100%;
 `
