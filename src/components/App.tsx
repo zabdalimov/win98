ebf8a2f79add94692b4f1f59b072aae1d82670c7
Zakir Abdalimov <zabdalimov@gmail.com>
2021-11-23 21:52:51 +0100

    App component extracted from index

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Provider as StoreProvider } from 'react-redux'
+import { store } from '../store/store'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from '../styles/theme'
+import MainWindow from './MainWindow/MainWindow'
+
+export const App: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <StoreProvider store={store}>
+        <ThemeProvider theme={theme}>
+          <MainWindow />
+        </ThemeProvider>
+      </StoreProvider>
+    </React.StrictMode>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
-import MainWindow from './components/MainWindow/MainWindow'
-import { Provider as StoreProvider } from 'react-redux'
-import { store } from './store/store'
-import { ThemeProvider } from '@emotion/react'
-import { theme } from './styles/theme'
+import { App } from './components/App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <StoreProvider store={store}>
-      <ThemeProvider theme={theme}>
-        <MainWindow />
-      </ThemeProvider>
-    </StoreProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
