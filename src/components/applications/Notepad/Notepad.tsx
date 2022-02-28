2b3a87a2f2741b6870817c3c79ecd39bb435fc91
Zakir Abdalimov <zabdalimov@gmail.com>
2022-02-28 21:14:39 +0100

    Notepad styling moved, proper height and width added
    Signed-off-by: Zakir Abdalimov <zabdalimov@gmail.com>

--- src/components/applications/Notepad/Notepad.tsx
+++ src/components/applications/Notepad/Notepad.tsx
@@ -1,8 +1,26 @@
+import styled from '@emotion/styled'
 import React from 'react'
 
+import mixins from '../../../styles/mixins.styles'
 import MenuBarEntry from '../../MenuBarEntry/MenuBarEntry'
 
-import { NotepadMenu, NotepadStyled, NotepadTextArea } from './Notepad.styles'
+const NotepadStyled = styled.div`
+  width: 600px;
+  height: 400px;
+
+  display: flex;
+  flex-direction: column;
+`
+
+const NotepadMenu = styled.div`
+  display: flex;
+`
+
+const NotepadTextArea = styled.textarea`
+  height: 100%;
+  resize: none;
+  ${mixins.standardBorderInvertedAlt}
+`
 
 const Notepad: React.FC = ({}) => {
   const menus = ['File', 'Edit', 'Search', 'Help']
