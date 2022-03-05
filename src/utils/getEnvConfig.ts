2d175368b7ab9ec783db4edf1e36e67cff64ad2a
Zakir Abdalimov <zabdalimov@gmail.com>
2022-03-05 03:12:53 +0100

    package version displayed in about application

--- src/utils/getEnvConfig.ts
+++ src/utils/getEnvConfig.ts
@@ -1,9 +1,11 @@
 const env = process.env.NODE_ENV
+const packageVersion = process.env.PACKAGE_VERSION
 const commitHash = process.env.COMMIT_HASH
 const projectUrl = process.env.PROJECT_URL
 
 export function getEnvConfig() {
   return {
+    packageVersion,
     commitHash,
     env,
     // isDevelopment: env === 'development',
