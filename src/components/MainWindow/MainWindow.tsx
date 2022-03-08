9db57fcfeaea6ae90da7fcb148d1f51042da8489
Zakir Abdalimov <zabdalimov@gmail.com>
2022-03-08 16:51:20 +0100

    MainWindowStyled moved to MainWindow

--- src/components/MainWindow/MainWindow.tsx
+++ src/components/MainWindow/MainWindow.tsx
@@ -1,8 +1,10 @@
+import styled from '@emotion/styled'
 import React from 'react'
 
 import { useBiosLoading } from '../../hooks/useBiosLoading'
 import { useShutDown } from '../../hooks/useShutDown'
 import { useWindowsLoading } from '../../hooks/useWindowsLoading'
+import cursorDefault from '../../static/cursor-default.png'
 import { BiosStartupScreen } from '../BiosStartupScreen/BiosStartupScreen'
 import DesktopGrid from '../DesktopGrid/DesktopGrid'
 import { ErrorBoundary } from '../ErrorBoundary/ErrorBoundary'
@@ -11,8 +13,22 @@ import { ShutDownScreen } from '../ShutDownScreen/ShutDownScreen'
 import TaskBar from '../TaskBar/TaskBar'
 import { WindowsStartupScreen } from '../WindowsStartupScreen/WindowsStartupScreen'
 
-import { MainWindowStyled } from './MainWindow.styles'
+const MainWindowStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1024px;
+  max-height: 768px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  cursor: url(${cursorDefault}), auto;
+  font-family: ${({ theme }) => theme.fontFamilies.windows95};
+  user-select: none;
 
+  img {
+    image-rendering: pixelated;
+  }
+`
 const MainWindow: React.FC = () => {
   const { isWindowsLoaded } = useWindowsLoading()
   const { isBiosLoaded } = useBiosLoading()
