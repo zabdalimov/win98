99bcd0a73b2705933f715e7a8adb0c9ba431693d
Zakir Abdalimov <zabdalimov@gmail.com>
2022-03-08 17:01:14 +0100

    useStartup renamed to useFakeLoading

--- src/hooks/useFakeLoading.tsx
+++ src/hooks/useFakeLoading.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 import { randomIntFromInterval } from '../utils/math'
 
-export default function useStartup(minTimeMs: number, maxTimeMs: number) {
+export function useFakeLoading(minTimeMs: number, maxTimeMs: number) {
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
