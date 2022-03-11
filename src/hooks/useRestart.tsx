dd2d04f7554e1fb43317d8a3cbb61ec9310d1085
Zakir Abdalimov <zabdalimov@gmail.com>
2022-03-11 21:35:34 +0100

    shutdown sound added

--- src/hooks/useRestart.tsx
+++ src/hooks/useRestart.tsx
@@ -1,3 +1,4 @@
+import { useAudio } from './useAudio'
 import { useResetState } from './useResetState'
 import { useSystemLoading } from './useSystemLoading'
 
@@ -5,11 +6,14 @@ export function useRestart() {
   const { resetState } = useResetState()
   const { setIsSystemLoading } = useSystemLoading()
 
+  const { playAudio } = useAudio()
+
   const restart = () => {
     setIsSystemLoading(true)
+    playAudio('shutdown.mp3')
     setTimeout(() => {
       resetState()
-    }, 3000)
+    }, 4500)
   }
 
   return {
--- src/hooks/useShutDown.tsx
+++ src/hooks/useShutDown.tsx
@@ -3,20 +3,24 @@ import { useDispatch, useSelector } from 'react-redux'
 import { changeIsShutDown } from '../store/system/actions'
 import { selectSystem } from '../store/system/selectors'
 
+import { useAudio } from './useAudio'
 import { useSystemLoading } from './useSystemLoading'
 
 export function useShutDown() {
   const { isShutDown } = useSelector(selectSystem)
   const { setIsSystemLoading } = useSystemLoading()
 
+  const { playAudio } = useAudio()
+
   const dispatch = useDispatch()
 
   const shutDown = () => {
     setIsSystemLoading(true)
+    playAudio('shutdown.mp3')
     setTimeout(() => {
       dispatch(changeIsShutDown(true))
       setIsSystemLoading(false)
-    }, 3000)
+    }, 4500)
   }
 
   return {
